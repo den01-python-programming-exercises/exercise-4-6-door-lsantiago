0c33b5758abb1134b4c4733b1cf391444ad389b6
Santiago Quiñones-Cuenca <lsquinones@gmail.com>
2020-12-25 01:17:36 +0000

    Solve Problem

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
+class Door:
+	def __init__(self):
+		pass
+	
+	def knock(self):
+		return "Who's there?"
+
 def main():
-    #write your code below this line
+	alexander = Door()
+	print(alexander.knock())
+	print(alexander.knock())
 
 if __name__ == '__main__':
-    main()
+	main()
